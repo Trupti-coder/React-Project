ab6d227822774f7975b85dc328efdd1ded582acd
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-04 13:23:37 -0500

    Update

--- reactprj/src/components/SignUp.js
+++ reactprj/src/components/SignUp.js
@@ -1,4 +1,15 @@
+import { useState } from "react";
+import styles from "./Signup.module.css";
+
 function SignUp(){
+
+    const [name, setName] = useState("");
+    const [number, setNumber] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const [success, setSuccess] = useState("");
+
     return(
         <>
         </>
