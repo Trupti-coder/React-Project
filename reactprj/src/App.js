93dca324dfda5d0f432c0fb1a4442d1228389871
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-04 13:45:02 -0500

    Update App.js

--- reactprj/src/App.js
+++ reactprj/src/App.js
@@ -1,9 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './components/Login';
 
 function App() {
 
   return (
     <>
+   <Routes>
+   <Route path='/signup' element={<Signup />} />
+   <Route path='/login' element={<Login />} />
+   </Routes>
     </>
    
   );
