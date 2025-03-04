09eec5655970b63938e1f3dc549af46b06609e5b
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-04 13:36:18 -0500

    Update Login.js

--- reactprj/src/components/Login.js
+++ reactprj/src/components/Login.js
@@ -20,8 +20,24 @@ function Login(){
                     />
                 </div>
 
+                <div>
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={passwordChange}
+                        placeholder="Enter your password"
+                        required
+                        className={styles.inputField}
+                    />
+                </div>
+                <button type="submit" className={styles.submitButton}>Login</button>
             </form>
 
+            <div style={{ marginTop: "10px" }}>
+                <p>If you don't have an account, <Link to="/signup">Sign up here</Link></p>
+            </div>
+
+        
         </div>
         </>
     );
