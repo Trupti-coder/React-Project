83bc9a24d6a423d16e5c7b972e6dd1ee921ae932
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-04 13:37:07 -0500

    Update

--- reactprj/src/components/Login.js
+++ reactprj/src/components/Login.js
@@ -1,5 +1,11 @@
-
+import React, { useState } from "react";
+import styles from "./Login.module.css";
 function Login(){
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const navigate = useNavigate();
     return(
         <>
         <div className={styles.signinForm}>
