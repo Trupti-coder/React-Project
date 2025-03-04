97052f0c6bd1d1a4b193718035cff92a434f3909
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-04 13:34:40 -0500

    Update Login.js

--- reactprj/src/components/Login.js
+++ reactprj/src/components/Login.js
@@ -1,6 +1,17 @@
+
 function Login(){
     return(
         <>
+        <div className={styles.signinForm}>
+            <form onSubmit={funSubmit}>
+
+            <h2>Login</h2>
+                
+                {error && <p className={styles.errorMessage}>{error}</p>}
+
+            </form>
+
+        </div>
         </>
     );
 }
